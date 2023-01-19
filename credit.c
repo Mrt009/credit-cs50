9bec8d46e9128ed42c8e2099061b495024ea7436
Mrt009 <matrikaregmi09@gmail.com>
2023-01-19 20:59:41 +0545

    initial commit

---
+++ credit.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+#include "credit_helper.h"
+
+int main(void)
+{
+    // 1. Get number from user
+    string cardnumber = get_cardnumber("Number: ");
+
+    // 2. Calculate the checksum
+    bool checksum = get_checksum(cardnumber);
+
+    // 3. Get card length and starting digits
+    int startingdigits = get_startingdigits(cardnumber);
+    int cardlength = get_cardlength(cardnumber);
+
+    // 4. Print AMEX, MASTERCARD, VISA, or INVALID
+    print_results(checksum, startingdigits, cardlength);
+}
+
+
+
